9eb40fd6fa4199a17dfc3f9d86866e3a5f49963c
admclamb <adylanmclamb@yahoo.com>
2026-04-09 23:47:49 -0400

    Update a bunch

--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { sql } from "drizzle-orm";
 import { pgTableCreator } from "drizzle-orm/pg-core";
+import postgres from "postgres";
 
 const createTable = pgTableCreator((name) => `ai-madlib-generator-v2_${name}`);
 
@@ -16,15 +16,13 @@ export const featureToggles = createTable("feature_toggles", (d) => ({
     .notNull(),
 }));
 
-dotenv.config({ path: "./.env" });
+const connectionString = process.env.DATABASE_URL;
 
-if (!("DATABASE_URL" in process.env))
+if (!connectionString)
   throw new Error("DATABASE_URL not found on .env");
 
 const main = async () => {
-  const client = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+  const client = postgres(connectionString);
   const db = drizzle(client);
   const data: (typeof featureToggles.$inferInsert)[] = [
     {
