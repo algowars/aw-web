59c32c76f9fdce6c2fd01a19e69e3203db59a445
admclamb <adylanmclamb@yahoo.com>
2026-04-12 18:12:22 -0400

    update e2e tests

--- cypress/e2e/account.cy.ts
+++ cypress/e2e/account.cy.ts
@@ -1,43 +1,30 @@
 describe("Accounts", () => {
   beforeEach(function () {
-    cy.intercept("GET", "/api/v1/account/find/profile").as("getProfile");
-    cy.intercept("POST", "/api/v1/account").as("createAccount");
     cy.intercept("POST", "/api/trpc/user.syncFromLogin*").as("syncFromLogin");
     cy.intercept("POST", "/api/trpc/user.getBySub*").as("getBySub");
 
     cy.visit("/");
   });
 
-  // it("Creating an account", () => {
-  //   cy.get("[data-cy=signup-btn]").click();
-
-  //   cy.generateRandomEmail().then((email) => {
-  //     cy.generateRandomPassword().then((password) => {
-  //       cy.signupViaAuth0Ui(email, password);
-  //     });
-  //   });
-
-  //   cy.wait("@getProfile").its("response.statusCode").should("eq", 404);
-  //   cy.url().should("include", "/account/setup");
-
-  //   cy.generateRandomUsername().then((username) => {
-  //     cy.get("[data-cy=username-input]").type(username);
-  //     cy.get("[data-cy=complete-setup-btn]").click();
-  //   });
-
-  //   cy.wait("@createAccount").its("response.statusCode").should("eq", 200);
-  //   cy.url().should("include", "/");
-  // });
-
-  it("Creating an account", () => {
+  it("creates an account, logs out, and logs back in", () => {
     cy.get("[data-cy=signup-btn]").click();
 
     cy.generateRandomEmail().then((email) => {
       cy.generateRandomPassword().then((password) => {
         cy.signupViaAuth0Ui(email, password);
+
+        cy.wait("@syncFromLogin").its("response.statusCode").should("eq", 200);
+        cy.url().should("include", "/dashboard");
+
+        cy.get("[data-cy=account-dropdown-trigger]").click();
+        cy.get("[data-cy=logout-btn]").click();
+
+        cy.get("[data-cy=login-btn]").click();
+        cy.loginViaAuth0Ui(email, password);
+
+        cy.wait("@syncFromLogin").its("response.statusCode").should("eq", 200);
+        cy.url().should("include", "/dashboard");
       });
     });
-
-    cy.url().should("include", "/account/setup");
   });
 });
--- cypress/support/auth-provider-commands/auth0.ts
+++ cypress/support/auth-provider-commands/auth0.ts
@@ -1,28 +1,36 @@
-function loginViaAuth0Ui(username: string, password: string) {
-  cy.visit("/");
-
+function loginViaAuth0Ui(
+  email: string,
+  password: string,
+  emailInputName = "email",
+) {
   cy.origin(
     Cypress.expose("auth0_domain"),
-    { args: { username, password } },
-    ({ username, password }) => {
-      cy.get("input#username").type(username);
+    { args: { email, password, emailInputName } },
+    ({ email, password, emailInputName }) => {
+      cy.get(`input#${emailInputName}`).type(email);
       cy.get("input#password").type(password, { log: false });
       cy.contains("button[value=default]", "Continue").click();
     },
   );
-
-  cy.url().should("equal", "http://localhost:3000/");
 }
 
-Cypress.Commands.add("loginToAuth0", (username: string, password: string) => {
+Cypress.Commands.add("loginViaAuth0Ui", (email: string, password: string) => {
+  loginViaAuth0Ui(email, password, "username"); // auth0 changes this to username on login
+});
+
+Cypress.Commands.add("signupViaAuth0Ui", (email: string, password: string) => {
+  loginViaAuth0Ui(email, password);
+});
+
+Cypress.Commands.add("loginToAuth0", (email: string, password: string) => {
   const log = Cypress.log({
     displayName: "AUTH0 LOGIN",
-    message: [`🔐 Authenticating | ${username}`],
+    message: [`🔐 Authenticating | ${email}`],
     autoEnd: false,
   });
   log.snapshot("before");
 
-  loginViaAuth0Ui(username, password);
+  loginViaAuth0Ui(email, password);
 
   log.snapshot("after");
   log.end();
