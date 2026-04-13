e1e3e03d8456643c928f08ed402901e2bec272dd
admclamb <adylanmclamb@yahoo.com>
2026-04-12 22:44:09 -0400

    Update

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,5 @@
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -8,28 +9,7 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `aw-web_${name}`);
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdById: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .$defaultFn(() => /* @__PURE__ */ new Date())
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [
-    index("created_by_idx").on(t.createdById),
-    index("name_idx").on(t.name),
-  ],
-);
-
-export const users = createTable("user", (d) => ({
+export const users = createTable("users", (d) => ({
   id: d
     .varchar({ length: 255 })
     .notNull()
@@ -39,4 +19,61 @@ export const users = createTable("user", (d) => ({
   name: d.varchar({ length: 255 }),
   email: d.varchar({ length: 255 }).notNull(),
   image: d.varchar({ length: 255 }),
+}));
+
+export const problemStatuses = createTable("problem_statuses", (d) => ({
+  id: d.integer().notNull().primaryKey(),
+  name: d.varchar({ length: 100 }).notNull().unique(),
+}));
+
+export const problems = createTable("problems", (d) => ({
+  id: d
+    .varchar({ length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  title: d.varchar({ length: 255 }).notNull().unique(),
+  question: d.text().notNull(),
+  statusId: d
+    .integer()
+    .notNull()
+    .references(() => problemStatuses.id),
+}));
+
+export const problemRelations = relations(problems, ({ one }) => ({
+  status: one(problemStatuses, {
+    fields: [problems.statusId],
+    references: [problemStatuses.id],
+  }),
+}));
+
+export const tags = createTable("tags", (d) => ({
+  id: d.integer().notNull().primaryKey(),
+  value: d.varchar({ length: 50 }).notNull().unique(),
+}));
+
+export const problemTags = createTable(
+  "problem_tags",
+  (d) => ({
+    problemId: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => problems.id),
+    tagId: d
+      .integer()
+      .notNull()
+      .references(() => tags.id),
+  }),
+  (t) => [primaryKey({ columns: [t.problemId, t.tagId] })],
+);
+
+export const problemsToTagsRelations = relations(problemTags, ({ one }) => ({
+  problem: one(problems, {
+    fields: [problemTags.problemId],
+    references: [problems.id],
+  }),
+  tag: one(tags, {
+    fields: [problemTags.tagId],
+    references: [tags.id],
+  }),
 }));
